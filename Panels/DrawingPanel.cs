5fbb5375dbf06c614f751b0db645db858ca23663
Quan Nguyen <nguyenquan263@gmail.com>
2023-09-30 11:30:38 -0500

    stupid super dummy commit

---
+++ Panels/DrawingPanel.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace NoobJumper.Panels
+{
+    public partial class DrawingPanel : Panel
+    {
+        private const int WIDTH = 50;
+        private const int HEIGHT = 50;
+        private int playerX;
+        private int playerY; 
+
+        public DrawingPanel()
+        {
+            this.ClientSize = new System.Drawing.Size(800, 600);
+            this.playerX = this.Width / 2 - 25;
+            this.playerY = this.Height - 100;
+
+            this.Focus();
+            this.DoubleBuffered = true;
+
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            Graphics g = e.Graphics;
+
+            int x = playerX;
+            int y = playerY;
+
+            int width = WIDTH;
+            int height = HEIGHT;
+
+            Brush b = new SolidBrush(Color.Blue);
+
+            g.FillRectangle(b, x, y, width, height);
+
+            b.Dispose();
+        }
+
+        public void MoveUp() 
+        {
+            this.playerY -= 5;
+            this.Invalidate();
+        }
+
+        public void MoveForward()
+        { 
+            this.playerX += 5;
+            this.Invalidate();
+        }
+
+        public void MoveBack()
+        {
+            this.playerX -= 5;
+            this.Invalidate();
+        }
+
+        public void MoveDown()
+        {
+            this.playerY += 5;
+            this.Invalidate();
+        }
+    }
+}
---
+++ Views/GameWindow.cs
@@ -0,0 +1,58 @@
+﻿using NoobJumper.Panels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace NoobJumper.Views
+{
+    public partial class GameWindow : Form
+    {
+        private DrawingPanel dp;
+
+        public GameWindow()
+        {
+            InitializeComponent();
+            this.KeyPreview = true;
+
+            this.Width = 800;
+            this.Height = 600;
+
+            dp = new DrawingPanel();
+            dp.Parent = this;
+            dp.Show();
+
+            this.KeyDown += ArrowButton_Click;
+            this.SizeChanged += DrawingPanel_SizeChanged;
+        }
+
+        private void DrawingPanel_SizeChanged(object sender, EventArgs e) 
+        {
+            dp.Size = this.ClientSize;
+        }
+
+        private void ArrowButton_Click(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode) 
+            { 
+                case Keys.Left:
+                    dp.MoveBack();
+                    return;
+                case Keys.Right:
+                    dp.MoveForward();
+                    return;
+                case Keys.Up:
+                    dp.MoveUp();
+                    return;
+                case Keys.Down:
+                    dp.MoveDown();
+                    return;
+                default:
+                    return;
+            }
+        }
+    }
+}
